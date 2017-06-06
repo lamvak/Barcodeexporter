902c2e5e75849a03a522907efc3d3e1eff9e47f7
Marcin Grabowski <lamvak@gmail.com>
2017-06-06 20:49:30 +0200

    Recognizing and marking barcode on sample screen

--- app/src/main/java/pl/lamvak/barcodeexporter/barcodeexporter/ShowBarcodeActivity.java
+++ app/src/main/java/pl/lamvak/barcodeexporter/barcodeexporter/ShowBarcodeActivity.java
@@ -1,17 +1,50 @@
 package pl.lamvak.barcodeexporter.barcodeexporter;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.SparseArray;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
-import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
-import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
-import java.security.MessageDigest;
+import static com.google.android.gms.vision.barcode.Barcode.CALENDAR_EVENT;
+import static com.google.android.gms.vision.barcode.Barcode.CODABAR;
+import static com.google.android.gms.vision.barcode.Barcode.CODE_128;
+import static com.google.android.gms.vision.barcode.Barcode.CODE_39;
+import static com.google.android.gms.vision.barcode.Barcode.CODE_93;
+import static com.google.android.gms.vision.barcode.Barcode.CONTACT_INFO;
+import static com.google.android.gms.vision.barcode.Barcode.DATA_MATRIX;
+import static com.google.android.gms.vision.barcode.Barcode.DRIVER_LICENSE;
+import static com.google.android.gms.vision.barcode.Barcode.EAN_13;
+import static com.google.android.gms.vision.barcode.Barcode.EAN_8;
+import static com.google.android.gms.vision.barcode.Barcode.EMAIL;
+import static com.google.android.gms.vision.barcode.Barcode.GEO;
+import static com.google.android.gms.vision.barcode.Barcode.ISBN;
+import static com.google.android.gms.vision.barcode.Barcode.ITF;
+import static com.google.android.gms.vision.barcode.Barcode.PDF417;
+import static com.google.android.gms.vision.barcode.Barcode.PHONE;
+import static com.google.android.gms.vision.barcode.Barcode.PRODUCT;
+import static com.google.android.gms.vision.barcode.Barcode.QR_CODE;
+import static com.google.android.gms.vision.barcode.Barcode.SMS;
+import static com.google.android.gms.vision.barcode.Barcode.TEXT;
+import static com.google.android.gms.vision.barcode.Barcode.UPC_A;
+import static com.google.android.gms.vision.barcode.Barcode.UPC_E;
+import static com.google.android.gms.vision.barcode.Barcode.URL;
+import static com.google.android.gms.vision.barcode.Barcode.WIFI;
 
 public class ShowBarcodeActivity extends AppCompatActivity {
     @Override
@@ -21,20 +54,66 @@ public class ShowBarcodeActivity extends AppCompatActivity {
 
         final ImageView imageView = (ImageView) findViewById(R.id.imageView);
 
+
         Glide.with(this)
                 .asBitmap()
                 .load(R.drawable.example_image_2)
-                .apply(RequestOptions.bitmapTransform(new BitmapTransformation() {
+                .listener(new RequestListener<Bitmap>() {
                     @Override
-                    protected Bitmap transform(@NonNull BitmapPool pool, @NonNull Bitmap toTransform, int outWidth, int outHeight) {
-                        return null;
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Bitmap> target, boolean isFirstResource) {
+                        return false;
                     }
 
                     @Override
-                    public void updateDiskCacheKey(MessageDigest messageDigest) {
+                    public boolean onResourceReady(Bitmap resource, Object model, Target<Bitmap> target, DataSource dataSource, boolean isFirstResource) {
+                        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(getApplicationContext())
+                                .setBarcodeFormats(
+                                    CODE_128 |
+                                    CODE_39 |
+                                    CODE_93 |
+                                    CODABAR |
+                                    DATA_MATRIX |
+                                    EAN_13 |
+                                    EAN_8 |
+                                    ITF |
+                                    QR_CODE |
+                                    UPC_A |
+                                    UPC_E |
+                                    PDF417 |
+                                    CONTACT_INFO |
+                                    EMAIL |
+                                    ISBN |
+                                    PHONE |
+                                    PRODUCT |
+                                    SMS |
+                                    TEXT |
+                                    URL |
+                                    WIFI |
+                                    GEO |
+                                    CALENDAR_EVENT |
+                                    DRIVER_LICENSE
+                        ).build();
+                        Log.i("INFO", "BarcodeDetector is operational: " + barcodeDetector.isOperational());
 
+                        Frame frame = new Frame.Builder()
+                                .setBitmap(resource).build();
+                        SparseArray<Barcode> barcodes = barcodeDetector.detect(frame);
+                        Log.i("INFO", "number of barcodes: " + barcodes.size());
+                        if (barcodes.size() > 0) {
+                            Barcode barcode = barcodes.get(barcodes.keyAt(0));
+                            Log.i("INFO", "Marking barcode " + barcode.rawValue);
+
+                            Canvas canvas = new Canvas(resource);
+                            Rect rect = barcode.getBoundingBox();
+                            Paint paint = new Paint();
+                            paint.setColor(Color.RED);
+                            paint.setStrokeWidth(19f);
+                            paint.setStyle(Paint.Style.STROKE);
+                            canvas.drawRect(rect, paint);
+                        }
+                        return false;
                     }
-                }))
+                })
                 .into(imageView);
     }
 }
