56b7cf1da6d538a79b6804d367190f291d48d060
Marcin Grabowski <lamvak@gmail.com>
2017-06-08 15:11:51 +0200

    Introduce proto and test SQlite read/write

--- app/src/main/java/pl/lamvak/barcodeexporter/barcodeexporter/CodesList.java
+++ app/src/main/java/pl/lamvak/barcodeexporter/barcodeexporter/CodesList.java
@@ -1,6 +1,10 @@
 package pl.lamvak.barcodeexporter.barcodeexporter;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.view.menu.ExpandedMenuView;
@@ -16,6 +20,9 @@ import java.util.ArrayList;
 
 public class CodesList extends AppCompatActivity {
     ArrayList<String> items = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    private SQLiteOpenHelper helper;
+    private SQLiteDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +33,45 @@ public class CodesList extends AppCompatActivity {
         final TextView editInput = (TextView) findViewById(R.id.inputItemText);
 
         ListView listView = (ListView) findViewById(R.id.barCodesListView);
-        final ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.list_item, items);
+        adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.list_item, items);
         listView.setAdapter(adapter);
+
+        helper = new SQLiteOpenHelper(getApplicationContext(), "barcodes", null, 1) {
+            @Override
+            public void onCreate(SQLiteDatabase db) {
+                db.execSQL("CREATE TABLE IF NOT EXISTS BARCODES (" +
+                        "CODE TEXT PRIMARY KEY ASC, " +
+                        "TOP REAL, " +
+                        "BOTTOM REAL, " +
+                        "LEFT REAL, " +
+                        "RIGHT REAL, " +
+                        "SOURCE_IMG_REF TEXT, " +
+                        "CREATION_TIMESTAMP INT ASC)");
+            }
+
+            @Override
+            public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            }
+        };
+
+        database = helper.getWritableDatabase();
+
+        Cursor barcodes = database.query("BARCODES", new String[]{"CODE"}, null, null, null, null, null, null);
+        while (barcodes.moveToNext()) {
+            Log.i("INFO", "reading data from db at row " + barcodes.getPosition());
+            String code = barcodes.getString(0);
+            addItemToModel(code);
+        }
 
         addItemButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CharSequence text = editInput.getText();
+                String text = editInput.getText().toString();
+                addItemToModel(text);
+                ContentValues values = new ContentValues();
+                values.put("CODE", text);
+                database.insert("BARCODES", null, values);
                 Log.i("INFO", "onClick: " + text);
-                items.add(text.toString());
-                adapter.notifyDataSetChanged();
             }
         });
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -44,5 +80,10 @@ public class CodesList extends AppCompatActivity {
                 startActivity(new Intent(CodesList.this, ShowBarcodeActivity.class));
             }
         });
+    }
+
+    private void addItemToModel(CharSequence text) {
+        items.add(text.toString());
+        adapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/test/java/pl/lamvak/barcodeexporter/ProtoBufferTest.java
@@ -0,0 +1,12 @@
+package pl.lamvak.barcodeexporter;
+
+import org.junit.Test;
+
+import pl.lamvak.barcodeexporter.proto.BarcodeExporterProtos;
+
+public class ProtoBufferTest {
+    @Test
+    public void createSimpleProto() {
+//        BarcodeExporterProtos.BarccodeOrBuilder
+    }
+}
